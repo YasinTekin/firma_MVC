12e5ec666d2927d6cf61ee66655c4c32fddb67a7
yasin <yasin@YASIN-PC>
2019-12-19 15:59:13 +0300

    Add project files.

---
+++ firma_MVC/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firma_MVC.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: About
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Firma | 2019 | Hakkımızda" ;
+            return View();
+        }
+    }
+}
